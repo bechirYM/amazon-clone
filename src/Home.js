5721a0b9738ea859599b7305e9ae19ace785685a
bechirYM <40782168+bechirYM@users.noreply.github.com>
2020-12-18 03:04:10 +0100

    master

---
+++ src/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/08/digital/video/gateway/placement/launch/TheWildsS1/WLDS_S1_GWBleedingHero_FT_COVIDUPDATE_XSite_1500X600_PV_fr-FR._CB413393199_.jpg"
+          alt=""
+        />
+
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+        
+        <div className="home__row">{/* Product */}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyADMpI2Js5J3CZioVaU5W746lxoEQw9qQo",
+    authDomain: "challenge-840df.firebaseapp.com",
+    projectId: "challenge-840df",
+    storageBucket: "challenge-840df.appspot.com",
+    messagingSenderId: "420973239831",
+    appId: "1:420973239831:web:277f076eea2320ef36c652",
+    measurementId: "G-4M67EBRPKE"
+  };
